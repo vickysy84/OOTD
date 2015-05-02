6358245884d44bdc5dfe010db8f166b54a92c113
Vicky Sy <vickysy84@gmail.com>
2015-05-02 11:05:28 +0800

    implemented ShareActionProvider (changed existing code from Azoft collage to turn off history)

--- app/src/main/java/com/redmadrobot/azoft/collage/ui/fragments/CollagePreviewFragment.java
+++ app/src/main/java/com/redmadrobot/azoft/collage/ui/fragments/CollagePreviewFragment.java
@@ -3,10 +3,19 @@ package com.redmadrobot.azoft.collage.ui.fragments;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.internal.view.SupportMenuItem;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,15 +29,23 @@ import com.mig35.injectorlib.utils.inject.InjectView;
 import com.redmadrobot.azoft.collage.data.CollageFillData;
 import com.redmadrobot.azoft.collage.exceptions.CollageCreationException;
 import com.redmadrobot.azoft.collage.loaders.CollagePreviewCreatorLoader;
-import com.redmadrobot.azoft.collage.utils.CommonUtils;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.vickysy.ootd.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Date: 4/9/2014
  * Time: 5:57 PM
  *
  * @author MiG35
+ *
+ * 5/2/2015
+ * Note: changed implementation to ShareActionProvider to turn off history.
+ * @author vickysy
  */
 public class CollagePreviewFragment extends ActionBarLoaderFragment {
 
@@ -45,6 +62,7 @@ public class CollagePreviewFragment extends ActionBarLoaderFragment {
     @InjectSavedState
     private String mResultPath;
 
+
     public CollagePreviewFragment() {
         setHasOptionsMenu(true);
         setMenuVisibility(true);
@@ -52,6 +70,7 @@ public class CollagePreviewFragment extends ActionBarLoaderFragment {
 
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_collage_preview, container, false);
     }
 
@@ -97,29 +116,82 @@ public class CollagePreviewFragment extends ActionBarLoaderFragment {
     private void setResultPath(final String resultPath) {
         mResultPath = resultPath;
 
-        Picasso.with(getActivity()).load(resultPath).fit().error(R.drawable.ic_action_new).skipMemoryCache().into(mCollageImageView);
+        Picasso.with(getActivity()).load(resultPath).fit().error(R.drawable.ic_action_new).
+                skipMemoryCache().into(mCollageImageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                // Setup share intent now that image has loaded
+                setupShareIntent();
+            }
+
+            @Override
+            public void onError() {
+            }
+        });
+
+
         getActionBarActivity().supportInvalidateOptionsMenu();
     }
 
-    @Override
-    public void onCreateOptionsMenu(final Menu menu, final MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
+    private final String[] INTENT_FILTER = new String[] {
+            "com.twitter.android",
+            "com.facebook.katana"
+    };
 
-        if (null != mResultPath) {
-            inflater.inflate(R.menu.collage_preview, menu);
+    ShareActionProvider mShareActionProvider;
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, final MenuInflater inflater) {
+        inflater.inflate(R.menu.collage_preview, menu);
+
+        // Set up ShareActionProvider's default share intent
+        MenuItem shareItem = menu.findItem(R.id.menu_share_collage);
+
+        mShareActionProvider = (ShareActionProvider)
+                MenuItemCompat.getActionProvider(shareItem);
+        if (mResultPath != null) {
+            ((SupportMenuItem) shareItem).setSupportActionProvider(mShareActionProvider);
         }
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(final MenuItem item) {
-        if (item.getItemId() == R.id.menu_share_collage) {
-            if (null != mResultPath) {
-                // because we use internal storage and provider, we should grand other apps permissions and so on...
-                CommonUtils.sendEmailWithAttachment(getActivity(), mResultPath, getString(R.string.text_email_subject));
-            }
-            return true;
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
         }
-        return super.onOptionsItemSelected(item);
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
+    public void setupShareIntent() {
+        // Fetch Bitmap Uri locally
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        ImageView ivImage = (ImageView) getActivity().findViewById(R.id.iv_collage);
+        Uri bmpUri = getLocalBitmapUri(ivImage);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        // Attach share event to the menu item provider
+        mShareActionProvider.setShareIntent(shareIntent);
     }
 
     public static Fragment getInstance(final CollageFillData collageFillData) {
@@ -136,8 +208,8 @@ public class CollagePreviewFragment extends ActionBarLoaderFragment {
         @Override
         public Dialog onCreateDialog(final Bundle savedInstanceState) {
             final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//            builder.setTitle(R.string.dialog_collage_creation_problem_title);
-//            builder.setMessage(R.string.dialog_collage_creation_problem_message);
+            builder.setTitle(R.string.dialog_collage_creation_problem_title);
+            builder.setMessage(R.string.dialog_collage_creation_problem_message);
             builder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(final DialogInterface dialog, final int which) {
